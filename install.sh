d55292d0db4e3bdcddf3efa599cce4cfe46dac82
Thomas Lent <tcl293@gmail.com>
2022-06-17 16:36:29 -0400

    Fix path

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 mkdir -p "$HOME/.zsh"
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 
-cat "./.zshrc" >> "$HOME/.zshrc"
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+cat "$script_dir/.zshrc" >> "$HOME/.zshrc"
