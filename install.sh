a11ae8f9f7c77eedc5b063faed308bf6865f66d0
Thomas Lent <tcl293@gmail.com>
2022-06-28 19:42:08 -0400

    Fix script_dir

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+
 git clone https://github.com/nushell/nushell.git "$HOME/nushell"
 cd "$HOME/nushell"
 rustup override set stable || true
@@ -10,7 +12,6 @@ cargo install \
         --root="/usr"
 chsh -s "/usr/bin/nu"
 
-script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 nu_config_dir="$HOME/.config/nushell"
 cp "$script_dir/config.nu" "$nu_config_dir"
 cp "$script_dir/env.nu" "$nu_config_dir"
